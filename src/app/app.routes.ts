6f1b09dface65da9c3329a2e18f8d9f9a773ede1
mhmod33 <me653830@gmail.com>
2026-07-01 15:47:13 +0300

    finished components and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent:()=>import('./components/dashboard/dashboard').then(m=>m.Dashboard),
+        pathMatch:'full'
+    },
+    {
+        path: 'dashboard',
+        loadComponent:()=>import('./components/dashboard/dashboard').then(m=>m.Dashboard),
+        pathMatch:'full'
+    },
+    {
+        path: 'agents',
+        loadComponent:()=>import('./components/agents/agents').then(m=>m.Agents),
+        pathMatch:'full'
+    },
+    {
+        path: 'orders',
+        loadComponent:()=>import('./components/orders/orders').then(m=>m.Orders),
+        pathMatch:'full'
+    },
+    {
+        path: 'properties',
+        loadComponent:()=>import('./components/properties/properties').then(m=>m.Properties),
+        pathMatch:'full'
+    },
+    {
+        path: 'settings',
+        loadComponent:()=>import('./components/settings/settings').then(m=>m.Settings),
+        pathMatch:'full'
+    },
+    {
+        path: 'users',
+        loadComponent:()=>import('./components/users/users').then(m=>m.Users),
+        pathMatch:'full'
+    },
+    {
+        path: '**',
+        loadComponent:()=>import('./components/notfound/notfound').then(m=>m.Notfound),
+        pathMatch:'full'
+    },
 
-export const routes: Routes = [];
+];
